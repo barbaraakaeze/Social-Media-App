07e21de9bd104117bad7c265576ac670400d449a
Barbara Akaeze <mailshonelle@yahoo.com>
2017-04-12 13:44:34 -0400

    Created UI for app

--- Ass03_7433881/ViewController.swift
+++ Ass03_7433881/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+   // @IBOutlet weak var description: UITextView!
+    @IBOutlet weak var currentLocation: UILabel!
+    @IBOutlet weak var dateTime: UILabel!
+    //Get photo from gallery
+    @IBAction func selectPhoto(_ sender: Any) {
+        let selectImage = UIImagePickerController()
+        selectImage.delegate = self
+        selectImage.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(selectImage, animated: true)
+        
+    }
+    //Save photo from gallery
+    @IBAction func saveBtn(_ sender: Any) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +37,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //Get image from gallery
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            imageView.image = selectImage
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
 
